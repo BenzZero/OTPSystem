48f1f5f148cfdd15553e1c838b81253b3183b937
BenzZero <banz_222@windowslive.com>
2020-02-12 23:29:13 +0700

    12/2v.1

--- api/src/api/files-apk/files-apk.controller.js
+++ api/src/api/files-apk/files-apk.controller.js
@@ -2,24 +2,37 @@ import express from 'express'
 import fs from 'fs'
 import path from 'path'
 import log4js from 'log4js'
-// import acceptsms from '../../files/apk/accept-sms.apk'
-// import erpsystem from '../../files/apk/erp-system.apk'
 const logErr = log4js.getLogger('error')
 const router = express.Router()
 
-const filesApk = async (req, res, next) => {
-  console.log(acceptsms)
-  console.log(erpsystem)
+const filesApkAll = async (req, res, next) => {
   try {
-    fs.readdir(path.resolve(__dirname, '..', 'public'), 'utf8', (err, files) => {
-      files.forEach((file) => console.info(file))
+    await fs.readdir(path.resolve(__dirname, '../../..', 'public/files/apk'), 'utf8', async (err, files) => {
+      return res.json({ files })
     })
-    return res.json({ success: success })
   } catch (e) {
     logErr.error(e)
     return res
   }
 }
 
-router.get('/', filesApk)
+const filesApkName = async (req, res, next) => {
+  try {
+    const { name } = req.params
+    let pathFile = path.join(__dirname, '../../..', `public/files/apk`, name)
+    return res.download(pathFile, name, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('downloading successful');
+      }
+    })
+  } catch (e) {
+    logErr.error(e)
+    return res
+  }
+}
+
+router.get('/:name', filesApkName)
+router.get('/', filesApkAll)
 module.exports = router
